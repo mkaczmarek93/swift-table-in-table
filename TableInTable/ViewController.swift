1abab2606db55c392f9e5cb9ab37b59821799c86
Michał Kaczmarek <michal.kaczmarek@comarch.pl>
2017-09-26 14:50:45 +0200

    Initial Commit

---
+++ TableInTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableInTable
+//
+//  Created by Michał Kaczmarek on 26.09.2017.
+//  Copyright © 2017 Michał Kaczmarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
